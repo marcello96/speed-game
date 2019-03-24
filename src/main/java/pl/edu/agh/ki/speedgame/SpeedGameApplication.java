bfe95f91f23b1eb67a68fb7c05f1f9b921b06e54
mjakubowski <Marcin.Jakubowski@grandparade.co.uk>
2019-03-24 11:03:44 +0100

    Initialize Spring Boot

---
+++ src/main/java/pl/edu/agh/ki/speedgame/SpeedGameApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.ki.speedgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedGameApplication.class, args);
+    }
+}
